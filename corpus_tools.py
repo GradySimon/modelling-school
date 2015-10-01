afa24690bc8d7ab66e8504c40b21742e073d2061
Grady Simon <grady@gradysimon.com>
2015-09-30 21:22:22 -0700

    Add cooccurrence_matrix method to Corpus

--- corpus_tools.py
+++ corpus_tools.py
@@ -1,16 +1,61 @@
 from itertools import repeat, chain
-from collections import Counter
+from collections import Counter, defaultdict
+from functools import partial
 import re
 import os
 import json
 import nltk
+from scipy.sparse import dok_matrix
+import numpy as np
 
 NULL_WORD = "<null>"
 
 class Corpus:
-    def __init__(self):
+    def __init__(self, **kwargs):
+        """
+        `size`: How many words on each side to select for each window. Specifying
+        `size` gives symmetric context windows and is equivalent to setting
+        `left_size` and `right_size` to the same value.
+        """
+        if 'size' in kwargs:
+            self.left_size = self.right_size = kwargs['size']
+        elif 'left_size' in kwargs or 'right_size' in kwargs:
+            self.left_size = kwargs.get('left_size', 0)
+            self.right_size = kwargs.get('right_size', 0)
+        else:
+            raise KeyError("At least one of `size`, `left_size`, and `right_size` must be given")
         self._words = None
         self._word_index = None
+        self._cooccurrence_matrix = None
+    
+    def tokenized_regions(self):
+        """
+        Returns an iterable of all tokenized regions of text from the corpus.
+        """
+        return map(self.tokenize, self.extract_regions())
+            
+    def fit(self):
+        word_counts = Counter()
+        cooccurrence_counts = defaultdict(Counter)
+        for region in self.tokenized_regions():
+            word_counts.update(region)
+            for left_context, word, right_context in self.region_context_windows(region):
+                cooccurrence_counts[word].update(chain(left_context, right_context))
+        self._words = [t[0] for t in word_counts.most_common()]
+        self._word_index = {word: i for i, word in enumerate(self._words)}
+        self._cooccurrence_matrix = dok_matrix((len(self._words), len(self._words)), dtype=np.uint32)
+        self._cooccurrence_matrix.update(
+            {(self._word_index[x], self._word_index[y]): count
+              for x, counts in cooccurrence_counts.items() for y, count in counts.items()
+              if y != NULL_WORD}
+        )
+
+    def is_fit(self):
+        """
+        Returns a boolean for whether or not the Corpus object has been fit to
+        the text yet.
+        """
+        return self._words is not None
 
     def extract_regions(self):
         """
@@ -28,37 +73,51 @@ class Corpus:
         """
         raise NotImplementedError()
     
-    def tokenized_regions(self):
+    @staticmethod
+    def window(region, start_index, end_index):
         """
-        Returns an iterable of all tokenized regions of text from the corpus.
+        Returns the list of words starting from `start_index`, going to `end_index`
+        taken from region. If `start_index` is a negative number, or if `end_index`
+        is greater than the index of the last word in region, this function will pad
+        its return value with `NULL_WORD`.
         """
-        return map(self.tokenize, self.extract_regions())
+        last_index = len(region) + 1
+        front_nulls = repeat(NULL_WORD, abs(min(start_index, 0)))
+        back_nulls = repeat(NULL_WORD, max(end_index - last_index, 0))
+        selected_tokens = region[max(start_index, 0) : min(end_index, last_index) + 1]
+        return list(chain(front_nulls, selected_tokens, back_nulls))
 
-    def _build_word_index(self):
-        tokens = chain.from_iterable(self.tokenized_regions())
-        word_counts = Counter(tokens)
-        self._words = [t[0] for t in word_counts.most_common()]
-        self._word_index = {word: i for i, word in enumerate(self._words)}
+    def region_context_windows(self, region):
+        for i, word in enumerate(region):
+            start_index = i - self.left_size
+            end_index = i + self.right_size
+            left_context = self.window(region, start_index, i - 1)
+            right_context = self.window(region, i + 1, end_index)
+            yield (left_context, word, right_context)
+
+    @property
+    def context_windows(self):
+        return (self.region_context_windows(region) for region in corpus.tokenized_regions())
 
     @property
     def words(self):
-        if self._words is None:
-            self._build_word_index()
+        if not self.is_fit():
+            self.fit()
         return self._words
     
     @property
     def word_index(self):
-        if self._word_index is None:
-            self._build_word_index()
+        if not self.is_fit():
+            self.fit()
         return self._word_index
 
-    def index_built(self):
-        """
-        Returns a boolean for whether or not the word index has been built yet.
-        """
-        return self._words is not None
+    @property
+    def cooccurrence_matrix(self):
+        if self._cooccurrence_matrix is None:
+            self.fit()
+        return self._cooccurrence_matrix
 
-    
+
 class RedditCorpus(Corpus):
     def __init__(self, path):
         """
@@ -89,40 +148,4 @@ class RedditCorpus(Corpus):
     @staticmethod
     def tokenize(string):
         return nltk.wordpunct_tokenize(string.lower())
-                    
-
-def window(region, start_index, end_index):
-    """
-    Returns the list of words starting from `start_index`, going to `end_index`
-    taken from region. If `start_index` is a negative number, or if `end_index`
-    is greater than the index of the last word in region, this function will pad
-    its return value with `NULL_WORD`.
-    """
-    last_index = len(region) + 1
-    front_nulls = repeat(NULL_WORD, abs(min(start_index, 0)))
-    back_nulls = repeat(NULL_WORD, max(end_index - last_index, 0))
-    selected_tokens = region[max(start_index, 0) : min(end_index, last_index) + 1]
-    return list(chain(front_nulls, selected_tokens, back_nulls))
-    
-
-def context_windows(corpus, **kwargs):
-    """
-    `size`: How many words on each side to select for each window. Specifying
-    `size` gives symmetric context windows and is equivalent to setting
-    `left_size` and `right_size` to the same value.
-    """
-    if 'size' in kwargs:
-        left_size = right_size = kwargs['size']
-    elif 'left_size' in kwargs or 'right_size' in kwargs:
-        left_size = kwargs.get('left_size', 0)
-        right_size = kwargs.get('right_size', 0)
-    else:
-        raise KeyError("At least one of `size`, `left_size`, and `right_size` must be given")
-    for region in corpus.tokenized_regions():
-        for i, word in enumerate(region):
-            start_index = i - left_size
-            end_index = i + right_size
-            left_context = window(region, start_index, i - 1)
-            right_context = window(region, i + 1, end_index)
-            yield (left_context, word, right_context)
 
